a18232e4ad5fc3efc9cf56c8086b67920c5bf598
vadimsukach <vadim.sukach@gmail.com>
2020-01-04 09:08:56 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Bar from './components/bar/bar';
+
+ReactDOM.render(
+    <Bar/>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.tsx',
+    },
+    output: {
+        filename: 'build/app.js',
+        path: path.resolve(__dirname, '/public'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        hot: true,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.less', '.css']
+    },
+    module: {
+        rules: [
+            {test: /\.tsx?$/, loader: 'ts-loader'},
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
+        ]
+
+    }
+};
